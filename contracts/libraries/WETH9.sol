4a62cb407f7b5797c2014ada2c12421156f6d114
tomguo <liuxingfeiyu@gmail.com>
2021-03-24 23:25:51 +0800

    update contracts for review

--- contracts/libraries/WETH9.sol
+++ contracts/libraries/WETH9.sol
@@ -22,8 +22,8 @@
 pragma solidity ^0.4.18;
 
 contract WETH9 {
-    string public name     = "Wrapped Ether";
-    string public symbol   = "WETH";
+    string public name     = "Wrapped OKT";
+    string public symbol   = "WOKT";
     uint8  public decimals = 18;
 
     event  Approval(address indexed src, address indexed guy, uint wad);
